94e1223975ed19259919dfd934e14eada47b7b30
Caique Siqueira <latorrecaique@gmail.com>
2022-12-26 14:54:38 -0300

    Entidades

---
+++ src/SME.SERAp.Prova.Item.Dominio/Entities/Disciplina.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Item.Dominio.Entities
+{
+    public class Disciplina : EntidadeBase
+    {
+
+        public Disciplina()
+        {
+
+        }
+
+        public Disciplina(long? id, long legadoId, string descricao, StatusGeral status)
+        {
+            if (id == null)
+            {
+                CriadoEm = AlteradoEm = DateTime.Now;
+                Status = (int)StatusGeral.Ativo;
+            }
+            else
+            {
+                Id = (long)id;
+                AlteradoEm = DateTime.Now;
+            }
+
+            LegadoId = legadoId;
+            Descricao = descricao;
+            Status = (int)status;
+        }
+
+        public long LegadoId { get; set; }
+        public string Descricao { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AlteradoEm { get; set; }
+        public int Status { get; set; }
+
+    }
+}
---
+++ src/SME.SERAp.Prova.Item.Dominio/Entities/Matriz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Item.Dominio.Entities
+{
+    public class Matriz : EntidadeBase
+    {
+
+        public Matriz()
+        {
+
+        }
+
+        public Matriz(long? id, long legadoId, string descricao, StatusGeral status)
+        {
+            if (id == null)
+            {
+                CriadoEm = AlteradoEm = DateTime.Now;
+                Status = (int)StatusGeral.Ativo;
+            }
+            else
+            {
+                Id = (long)id;
+                AlteradoEm = DateTime.Now;
+            }
+
+            LegadoId = legadoId;
+            Descricao = descricao;
+            Status = (int)status;
+        }
+
+        public long LegadoId { get; set; }
+        public string Descricao { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AlteradoEm { get; set; }
+        public int Status { get; set; }
+
+        //public Disciplina DisciplinaId { get; set; }
+    }
+}
